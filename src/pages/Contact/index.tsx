682332f0eb69db3c44d7081ab29f844ef7534396
Fabrício <fabricio_c_anelli@hotmail.com>
2023-01-24 19:48:12 -0300

    update

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,5 +1,51 @@
 import { Header } from "../../components/Header";
+import { Container } from "./style";
+import location from "../../assets/location.png";
+import locationsvg from "../../assets/location.svg";
+import mail from "../../assets/mail.svg";
 
 export const Contact = () => {
-  return <Header />;
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <div className="infos animeLeft">
+          <img src={locationsvg} alt="effect" className="effect" />
+          <img src={mail} alt="maileffect" className="maileffect" />
+          <form onSubmit={handleSubmit}>
+            <div className="description">
+              <h1 className="title">Contact Us</h1>
+              <p>Our Friendly team would love to hear from you</p>
+            </div>
+            <div className="name">
+              <span>
+                <label>First Name</label>
+                <input type="text" name="first" placeholder="First Name" />
+              </span>
+              <span>
+                <label>Last Name</label>
+                <input type="text" name="last" placeholder="Last Name" />
+              </span>
+            </div>
+            <span>
+              <label>E-mail</label>
+              <input type="email" name="email" placeholder="you@company.com" />
+            </span>
+            <span>
+              <label>Message</label>
+              <textarea name="message" cols={30} rows={10}></textarea>
+            </span>
+            <button>Send Message</button>
+          </form>
+        </div>
+        <div>
+          <img src={location} alt="location" className="animeRight" />
+        </div>
+      </Container>
+    </>
+  );
 };
---
+++ src/pages/Contact/style.ts
@@ -0,0 +1,109 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  justify-content: center;
+  padding: 2rem;
+  margin-top: 4rem;
+  color: #313131;
+
+  .infos {
+    padding: 2rem;
+    position: relative;
+
+    .effect {
+      position: absolute;
+      width: 40px;
+      top: 120px;
+      right: 80px;
+      opacity: 0.8;
+      transform: rotate(-45deg);
+    }
+
+    .maileffect {
+      position: absolute;
+      width: 40px;
+      top: -20px;
+      left: 20px;
+      transform: rotate(45deg);
+    }
+
+    .title {
+      font-size: 3rem;
+      line-height: 4rem;
+    }
+
+    .description {
+      margin-bottom: 2rem;
+    }
+  }
+
+  label {
+    font-size: 0.9rem;
+    font-weight: 400;
+    margin-bottom: 0.25rem;
+  }
+
+  input {
+    height: 40px;
+    border: 0.5px solid #ccc;
+    border-radius: 5px;
+    padding: 1rem;
+    font-size: 0.9rem;
+    font-weight: 300;
+    font-family: "Poppins", sans-serif;
+    outline: none;
+    background-color: #f9f6f1;
+
+    &:focus {
+      border: 0.5px solid #313131;
+    }
+  }
+
+  textarea {
+    height: 95px;
+    border: 0.5px solid #ccc;
+    border-radius: 5px;
+    padding: 1rem;
+    font-size: 0.9rem;
+    font-weight: 300;
+    font-family: "Poppins", sans-serif;
+    outline: none;
+    resize: none;
+    background-color: #f9f6f1;
+
+    &:focus {
+      border: 0.5px solid #313131;
+    }
+  }
+
+  button {
+    height: 40px;
+    border: 0;
+    border-radius: 5px;
+    background-color: #313131;
+    color: #fff;
+    cursor: pointer;
+    &:hover {
+      background-color: #000;
+    }
+  }
+
+  .name {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 2rem;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  span {
+    display: flex;
+    flex-direction: column;
+  }
+`;
